bb2a6247c8c33921eb2ab449b2d57ee0665c1ba2
Almat Akhat <fabiannsailor@gmail.com>
2022-02-10 01:56:07 +0600

    added simple form

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model="link" />
+    <button @click.prevent="send()">send</button><br />
+    <a :href="this.info">{{ info }}</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      link: "",
+      info: "",
+    };
+  },
+  mounted() {},
+  methods: {
+    send() {
+      const host = "https://gb-backend1-coursework.herokuapp.com";
+      let data = { original_url: this.link };
+      console.log(data);
+      axios
+        .post(host, data)
+        .then((response) => {
+          console.log(response);
+          this.info = response.data.short_url;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +45,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.link {
+  font-size: 16px;
+  line-height: 130%;
 }
 </style>
